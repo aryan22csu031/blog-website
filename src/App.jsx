123e39d0250d21eb02caab78dd7fa1b9b787ff12
aryan22csu031 <aryan22csu031@ncuindia.edu>
2024-06-05 22:00:33 +0530

    added post functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth';
+import {login,logout} from './store/authSlice'
+import {Header,Footer} from './components'
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL)
+  const [Loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login(userData))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>{setLoading(false)})
+  },[])
 
-  return (
-    <>
-      <h1>A blog Website</h1>
-    </>
-  )
+  return !Loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-900'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          TODO{/* <Outlet/> */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ) : null
 }
 
 export default App
